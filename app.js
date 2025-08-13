702a1c73a0885e9e19483d3419035eff0efa184d
Eduardo Henrique <henrique.idk@gmail.com>
2025-08-12 21:10:14 -0300

    loop para percorrer a lista + criar um elemento para cada amigo nela

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let amigos = []; // armazenar os nomes
 let lista = document.getElementById('listaAmigos'); // lista no HTML onde os nomes vão aparecer
 
 function adicionarAmigo() { // função para o botão "Adicionar"
-    let nomeDoAmigo = document.getElementById('amigo').value.trim(); // pega o valor do input e retira os espaços vazios
+    let nomeDoAmigo = document.getElementById('amigo').value.trim(); // pega o valor do input + retira os espaços vazios [NÃO ENSINADO]
 
     if (nomeDoAmigo === '') { // verifica se o campo está vazio
         alert('Por favor, insira um nome!'); // notifica se estiver vazio
@@ -11,4 +11,8 @@ function adicionarAmigo() { // função para o botão "Adicionar"
     amigos.push(nomeDoAmigo); // adiciona o nome à lista
     document.getElementById('amigo').value = ""; // limpa o campo de input
     lista.innerHTML = ""; // limpa a lista pra evitar duplicatas
+
+    for (let i = 0; i < amigos.length; i++) { // loop que percorre toda a lista de amigos [NÃO ENSINADO]
+    lista.innerHTML += `<li>${amigos[i]}</li>`; // Adiciona cada nome como um <li> na lista [NÃO ENSINADO]
+    }
 }
